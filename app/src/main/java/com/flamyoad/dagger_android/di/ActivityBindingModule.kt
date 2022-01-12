74940bc321a8eeba64c19669177010ca4226b670
flamyoad <yizake1820@gmail.com>
2022-01-12 13:48:12 +0800

    Setup data binding

--- app/src/main/java/com/flamyoad/dagger_android/di/ActivityBindingModule.kt
+++ app/src/main/java/com/flamyoad/dagger_android/di/ActivityBindingModule.kt
@@ -2,6 +2,7 @@ package com.flamyoad.dagger_android.di
 
 import com.flamyoad.dagger_android.MainActivity
 import com.flamyoad.dagger_android.di.login.LoginModule
+import com.flamyoad.dagger_android.di.scope.ActivityScope
 import com.flamyoad.dagger_android.ui.login.LoginActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -9,11 +10,13 @@ import dagger.android.ContributesAndroidInjector
 @Module
 abstract class ActivityBindingModule {
     @ContributesAndroidInjector
+    @ActivityScope
     abstract fun contributeMainActivity(): MainActivity
 
-    @ContributesAndroidInjector(
-        modules = [LoginModule::class]
-    )
+    @ContributesAndroidInjector(modules = [
+        LoginModule::class
+    ])
+    @ActivityScope
     abstract fun contributeLoginActivity(): LoginActivity
 
 }
