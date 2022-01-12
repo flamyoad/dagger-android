4a51158dbcfa020526b62dc933002a9229c0776d
flamyoad <yizake1820@gmail.com>
2022-01-12 12:28:35 +0800

    Implement base classes

---
+++ app/src/main/java/com/flamyoad/dagger_android/common/BaseFragment.kt
@@ -0,0 +1,7 @@
+package com.flamyoad.dagger_android.common
+
+import androidx.fragment.app.Fragment
+import com.flamyoad.dagger_android.di.Injectable
+
+open class BaseFragment: Fragment(), Injectable {
+}
